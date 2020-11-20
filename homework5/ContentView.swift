c347e2260b44e39a94465b0a988d88eabe687257
aaaaaaaaaaa <aaaaa>
2020-11-20 18:47:55 +0800

    Initial Commit

---
+++ homework5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework5
+//
+//  Created by User09 on 2020/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework5/homework5App.swift
@@ -0,0 +1,17 @@
+//
+//  homework5App.swift
+//  homework5
+//
+//  Created by User09 on 2020/11/20.
+//
+
+import SwiftUI
+
+@main
+struct homework5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
